63e2a71669540f9619f46092715e604f26946f0f
Jo Grimstad <jo@literacyapp.org>
2017-03-05 12:07:00 +0300

    Initial commit

---
+++ app/src/main/java/org/literacyapp/handwriting/MainActivity.java
@@ -0,0 +1,21 @@
+package org.literacyapp.handwriting;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Log.i(getClass().getName(), "onCreate");
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        Log.i(getClass().getName(), "onStart");
+        super.onStart();
+    }
+}
